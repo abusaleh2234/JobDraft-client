9a1bbcc9cccfb595d36038d2ef1484a1c5c543f8
abusaleh2234 <you@example.com>
2023-11-13 16:32:16 +0600

    add readme file

--- src/Pages/UpdateJobs/UpdateJobs.jsx
+++ src/Pages/UpdateJobs/UpdateJobs.jsx
@@ -110,7 +110,7 @@ const UpdateJobs = () => {
 
                     </div>
                     <div className="w-full">
-                        <button type="submit" className="btn w-full text-white right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Apply</button>
+                        <button type="submit" className="btn w-full text-white right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
                     </div>
                 </form>
             </div>
